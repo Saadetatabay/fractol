5eebd1635b1c523fda1bc80b66625bdba2be281a
saadetatabay <etatabaysaad143@gmail.com>
2025-08-25 16:15:42 +0300

    fractol arguman check

---
+++ fractol.c
@@ -0,0 +1,11 @@
+#include "ft_fractol.h"
+
+int main(int arg,char *argv[])
+{
+    if ((arg == 2 & !(argv[1],"mandelbrot",10)) || (arg == 4 && !ft_strncmp(argv[1],"julia")))
+    {
+
+    }
+    else
+        return (1);
+}
