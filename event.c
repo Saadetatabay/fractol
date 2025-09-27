0f0183eb4fd2448c0fad15ec0c89e2c0cfc9d905
Saadet Yuksel Atabay <satabay@k1m04s06.42istanbul.com.tr>
2025-09-27 17:45:44 +0300

    atof

--- event.c
+++ event.c
@@ -6,7 +6,7 @@
 /*   By: satabay <satabay@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 15:26:07 by satabay           #+#    #+#             */
-/*   Updated: 2025/09/27 16:04:07 by satabay          ###   ########.fr       */
+/*   Updated: 2025/09/27 17:45:27 by satabay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,4 +55,47 @@ void    event_init(t_fractal *farcatl)
 	mlx_hook(farcatl->mlx_wind, 2, 1L << 0, key, farcatl); //keypress
 	//mlx_hook(farcatl->mlx_wind,4,1L<<2,mouse,farcatl); //button press
 	mlx_hook(farcatl -> mlx_wind, 17, 1L << 17, my_close, farcatl); //destroy notify
+}
+
+double ft_atof(const char *str)
+{
+	double	result;
+	double	result1;
+	int	i;
+	int sign;
+
+	result = 0.0;
+	result1 = 0.0;
+	int	temp;
+	int j;
+	temp = 0;
+	i = 0;
+	sign = 1;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while(str[i] && str[i]!='.')
+	{
+		result*=10;
+		result += str[i] - '0';
+		i++;	
+	}
+	if(str[i] == '.')
+		i++;
+	j = 0;
+	while(str[i])
+	{
+		temp = str[i] - '0';
+		result1 = result1 * 10 + temp ;
+		i++;
+		j++;
+	}
+	while (j--)
+	{
+		result1 *= 0.1;
+	}
+	
+	return(result + result1);
 }
