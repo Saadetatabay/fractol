5211bd7a7a45c046b1f840bd92df25fec493bcf3
Saadetatabay <etatabaysaad143@gmail.com>
2025-10-02 12:07:44 +0300

    norminette

--- ft_fractol.h
+++ ft_fractol.h
@@ -9,46 +9,46 @@
 # include <X11/keysym.h>
 # include "minilibx-linux/mlx.h"
 
-#define WIDTH 800
-#define HEIGHT 800
+# define WIDTH 800
+# define HEIGHT 800
 
-#define BLACK       0x000000
-#define WHITE       0xFFFFFF
-#define RED         0xFF0000
-#define GREEN       0x00FF00
-#define BLUE        0x0000FF
-#define YELLOW      0xFFFF00
-#define MAGENTA     0xFF00FF
-#define CYAN        0x00FFFF
+# define BLACK       0x000000
+# define WHITE       0xFFFFFF
+# define RED         0xFF0000
+# define GREEN       0x00FF00
+# define BLUE        0x0000FF
+# define YELLOW      0xFFFF00
+# define MAGENTA     0xFF00FF
+# define CYAN        0x00FFFF
 
-#define RIGHT	65363
-#define LEFT	65361
-#define UP		65362
-#define DOWN	65364
-//#define ZOOM_IN 65451
-//#define ZOOM_OUT 65453
+# define RIGHT	65363
+# define LEFT	65361
+# define UP		65362
+# define DOWN	65364
 
 typedef struct s_num
 {
 	double	real_x;
 	double	imaginary_y;
-}t_num;
+}	t_num;
 
-typedef struct s_img{
+typedef struct s_img
+{
 	void	*img_ptr;
 	char	*pix_ptr;
 	int		line;
 	int		endian;
 	int		bit;
-}t_img;
+}	t_img;
 
-typedef struct s_fractol{
-    void	*mlx_conn;
-	void	*mlx_wind;
-	t_img	img;
-	char	*name;
-	double	escape_value;
-	int		iteration;
+typedef struct s_fractol
+{
+	void		*mlx_conn;
+	void		*mlx_wind;
+	t_img		img;
+	char		*name;
+	double		escape_value;
+	int			iteration;
 	double		a;
 	double		b;
 	float		zomm_in;
@@ -56,18 +56,17 @@ typedef struct s_fractol{
 	double		julia_i;
 	t_num		z;
 	t_num		c;
+}	t_fractal;
 
-}t_fractal;
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	ft_putstr_fd(char *s, int fd);
-void draw(t_fractal *fractol);
-void    event_init(t_fractal *farcatl);
-int my_close(t_fractal *fractol);
-int key(int keysym,t_fractal *fractol);
+void	draw(t_fractal *fractol);
+void	event_init(t_fractal *farcatl);
+int		my_close(t_fractal *fractol);
+int		key(int keysym, t_fractal *fractol);
 double	scale(double num, double new_min, double new_max, double old_max);
 void	mandel_vs_julia(int x, int y, t_fractal *fractol);
-double ft_atof(const char *str);
-void	params_error();
+double	ft_atof(const char *str);
+void	params_error(void);
 
 #endif
